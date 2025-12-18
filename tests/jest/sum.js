9f59cba4138a6023c08db8fe95847a3df040bc1d
Sanjay Kumar Saini <sanjaysaini4423@gmail.com>
2025-12-18 23:14:16 +0530

    Add daysBetweenDates function to sum.js

--- tests/jest/sum.js
+++ tests/jest/sum.js
@@ -1,3 +1,9 @@
+function daysBetweenDates(date1, date2) {
+  const oneDay = 24 * 60 * 60 * 1000;
+  return Math.round((date2 - date1) / oneDay);
+}
+
+module.exports = daysBetweenDates;
 
 function sum(a, b) {
   return a + b;
